228e912d80c24e405f58d93c8bd629200c0b084f
William Chan <william.chan@kindredgroup.com>
2025-04-11 16:45:53 +1000

    Add documentation to cache control output cache policy

--- CacheTest/CacheControlOutputCachePolicy.cs
+++ CacheTest/CacheControlOutputCachePolicy.cs
@@ -2,8 +2,15 @@
 
 namespace CacheTest
 {
+    /// <summary>
+    /// A policy to respect the no-cache and no-store directives of the Cache-Control request header
+    /// </summary>
     public class CacheControlOutputCachePolicy : IOutputCachePolicy
     {
+        /// <summary>
+        /// Disallows cache lookup and storage following the no-cache and no-store directives in the Cache-Control request header
+        /// </summary>
+        /// <inheritdoc/>
         public ValueTask CacheRequestAsync(OutputCacheContext context, CancellationToken cancellation)
         {
             if (context.HttpContext.Request.GetTypedHeaders().CacheControl?.NoCache == true)
@@ -19,11 +26,13 @@ namespace CacheTest
             return ValueTask.CompletedTask;
         }
 
+        /// <inheritdoc/>
         public ValueTask ServeFromCacheAsync(OutputCacheContext context, CancellationToken cancellation)
         {
             return ValueTask.CompletedTask;
         }
 
+        /// <inheritdoc/>
         public ValueTask ServeResponseAsync(OutputCacheContext context, CancellationToken cancellation)
         {
             return ValueTask.CompletedTask;
